7779208b136737cbc1e850e2b43c2aff53d155f8
xufan.zhang <sunshinezxf@hotmail.com>
2018-06-11 17:36:02 +0800

    initilization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import LoginPage from './component/login/login_page'
+
+class App extends Component {
+    render() {
+        return (
+            <LoginPage></LoginPage>
+        );
+    }
+}
+
+export default App;
---
+++ src/component/login/login_page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import gmair_blue from '../../logo/gmair_blue.png'
+
+import 'antd-mobile/dist/antd-mobile.css'
+
+import {InputItem, Button, WhiteSpace} from 'antd-mobile';
+
+const LoginPage = () => (
+    <div className="gmair_page">
+        <div className="gmair_logo_area">
+            <img src={gmair_blue} className="gmair_logo" alt="gmair_logo"></img>
+        </div>
+        <div className="gmair_logo_slogan">果麦新风，唤醒新鲜空气</div>
+        <div>
+            <InputItem type="phone" placeholder="请输入手机号码">手机号码</InputItem>
+            <InputItem placeholder="请输入动态密码" extra={<a>发送</a>}>短信验证</InputItem>
+        </div>
+        <WhiteSpace />
+        <Button type="primary">登录</Button>
+    </div>
+)
+
+export default LoginPage
