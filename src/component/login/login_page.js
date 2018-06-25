3059ca0011ac5980f2f5da5cadd91c6222d25e81
xufan.zhang <sunshinezxf@hotmail.com>
2018-06-25 18:33:30 +0800

    add login page

--- src/component/login/login_page.js
+++ src/component/login/login_page.js
@@ -1,24 +1,95 @@
 import React from 'react'
 
-import gmair_blue from '../../material/logo/gmair_blue.png'
+import ReactRevealText from 'react-reveal-text'
 
-import 'antd-mobile/dist/antd-mobile.css'
+import {Button} from 'react-bootstrap'
 
-import {InputItem, Button, WhiteSpace} from 'antd-mobile';
+import {FormGroup} from 'react-bootstrap'
 
-const LoginPage = () => (
-    <div className="gmair_page">
-        <div className="gmair_logo_area">
-            <img src={gmair_blue} className="gmair_logo" alt="gmair_logo"></img>
-        </div>
-        <div className="gmair_logo_slogan">果麦新风，唤醒新鲜空气</div>
-        <div>
-            <InputItem type="phone" placeholder="请输入手机号码">手机号码</InputItem>
-            <InputItem placeholder="请输入动态密码" extra={<a>发送</a>}>短信验证</InputItem>
-        </div>
-        <WhiteSpace />
-        <Button type="primary">登录</Button>
-    </div>
-)
+import {FormControl} from 'react-bootstrap'
+
+import {InputGroup} from 'react-bootstrap'
+
+import login from './login.png'
+
+import gmair_white from '../../material/logo/gmair_white.png'
+
+const gmair_login_page = {
+    backgroundImage: `url(${login})`,
+    width: `100%`,
+    height: `100%`,
+    backgroundSize: `100% 100%`
+}
+
+const logo = {
+    height: `6rem`,
+    width: `14rem`
+}
+
+const white_icon = {
+    color: `white`,
+    backgroundColor: `transparent`,
+    border: `unset`
+}
+
+const transparent_input = {
+    backgroundColor: `transparent`,
+    color: `#FFFFFF`,
+    border: `1px solid transparent`,
+    borderBottom: `1px solid #FFFFFF`,
+    boxShadow: `unset`,
+    width: `100%`
+}
+
+const login_btn = {
+    color: `#00AEEF`,
+    fontSize: `1.4rem`,
+    backgroundColor: `#FFFFFF`,
+    textAlign: `center`
+}
+
+
+class LoginPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showtext: false
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({showtext: true});
+        }, 1300);
+    }
+
+    render() {
+        return (
+            <div style={gmair_login_page}>
+                <div className="gmair_logo">
+                    <img src={gmair_white} style={logo} alt="GMAIR_LOGO"></img>
+                </div>
+                <ReactRevealText className="gmair_logo_slogan" show={this.state.showtext}>唤醒新鲜空气</ReactRevealText>
+                <div className="gmair_login_area">
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon style={white_icon}><span className="glyphicon glyphicon-phone"></span></InputGroup.Addon>
+                            <FormControl type="tel" placeholder = '请输入手机号码' style={transparent_input}></FormControl>
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon style={white_icon}><span className="glyphicon glyphicon-lock"></span></InputGroup.Addon>
+                            <FormControl type="text" placeholder = '请输入密码' style={transparent_input}></FormControl>
+                        </InputGroup>
+                    </FormGroup>
+                </div>
+                <div className="gmair_login_btn">
+                    <Button block style={login_btn}>登&nbsp;录</Button>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default LoginPage
