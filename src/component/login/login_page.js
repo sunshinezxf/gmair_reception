71d65ac201424e96b263bf152e3158aaff06ef3f
xufan.zhang <sunshinezxf@hotmail.com>
2018-06-26 11:12:23 +0800

    refactor slogan

--- src/component/login/login_page.js
+++ src/component/login/login_page.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import ReactRevealText from 'react-reveal-text'
-
 import {Button} from 'react-bootstrap'
 
 import {FormGroup} from 'react-bootstrap'
@@ -9,6 +7,8 @@ import {FormGroup} from 'react-bootstrap'
 import {FormControl} from 'react-bootstrap'
 
 import {InputGroup} from 'react-bootstrap'
+
+import Slogan from '../slogan/slogan'
 
 import login from './login.png'
 
@@ -41,6 +41,11 @@ const transparent_input = {
     width: `100%`
 }
 
+const password_btn = {
+    color: `#00AEEF`,
+    opacity: `0.75`
+}
+
 const login_btn = {
     color: `#00AEEF`,
     fontSize: `1.4rem`,
@@ -50,17 +55,14 @@ const login_btn = {
 
 
 class LoginPage extends React.Component {
-    constructor() {
-        super();
+    constructor(props, context) {
+        super(props, context);
+        this.read_mobile = this.read_mobile.bind(this);
         this.state = {
-            showtext: false
+            showtext: false,
+            mobile: '',
+            password: ''
         };
-    }
-
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({showtext: true});
-        }, 1300);
     }
 
     render() {
@@ -69,18 +71,19 @@ class LoginPage extends React.Component {
                 <div className="gmair_logo">
                     <img src={gmair_white} style={logo} alt="GMAIR_LOGO"></img>
                 </div>
-                <ReactRevealText className="gmair_logo_slogan" show={this.state.showtext}>唤醒新鲜空气</ReactRevealText>
+                <Slogan></Slogan>
                 <div className="gmair_login_area">
                     <FormGroup>
                         <InputGroup>
                             <InputGroup.Addon style={white_icon}><span className="glyphicon glyphicon-phone"></span></InputGroup.Addon>
-                            <FormControl type="tel" placeholder = '请输入手机号码' style={transparent_input}></FormControl>
+                            <FormControl type="tel" placeholder = '请输入手机号码' style={transparent_input} value={this.state.mobile} onChange={this.read_mobile}></FormControl>
                         </InputGroup>
                     </FormGroup>
                     <FormGroup>
                         <InputGroup>
                             <InputGroup.Addon style={white_icon}><span className="glyphicon glyphicon-lock"></span></InputGroup.Addon>
-                            <FormControl type="text" placeholder = '请输入密码' style={transparent_input}></FormControl>
+                            <FormControl type="password" placeholder = '请输入密码' style={transparent_input}></FormControl>
+                            <InputGroup.Addon style={password_btn}>获取验证码</InputGroup.Addon>
                         </InputGroup>
                     </FormGroup>
                 </div>
@@ -89,6 +92,17 @@ class LoginPage extends React.Component {
                 </div>
             </div>
         )
+    }
+
+    read_mobile = (e) => {
+        this.setState({mobile: e.target.value}, this.validate_mobile);
+    }
+
+    validate_mobile = () => {
+        var pattern = /^((\+?86)|(\+86\+86))?1\d{10}$/;
+        if (pattern.test(this.state.mobile) === true) {
+            console.log("mobile number received.");
+        }
     }
 }
 
---
+++ src/component/slogan/slogan.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import ReactRevealText from 'react-reveal-text'
+
+class Slogan extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            showtext: true
+        };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({showtext: !this.state.showtext});
+        }, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+           return <ReactRevealText className='gmair_logo_slogan' show={this.state.showtext}>唤醒新鲜空气</ReactRevealText>
+    }
+}
+
+export default Slogan;
