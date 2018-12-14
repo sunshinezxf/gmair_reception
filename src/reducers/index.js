8f8f26e21dbfdc6dbd30d78a1ab41f2842b97644
141190114@smail.nju.edu.cn <40664153+lawrenceZL@users.noreply.github.com>
2018-12-14 18:16:42 +0800

    combine_reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+// import login_reducer from './login_reducer';
+// import register_reducer from './register_reducer';
+// import machine_reducer from './register_reducer';
+import operation_reducer from './operation_reducer';
+
+export default combineReducers({
+    // login_reducer:login_reducer,
+    // register_reducer:register_reducer,
+    // machine_reducer:machine_reducer,
+    operation_reducer:operation_reducer,
+})
